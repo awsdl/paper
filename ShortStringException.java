3b729763b8986fe0829fe9ea66b3b180e8678a19
awsdl <144198566+awsdl@users.noreply.github.com>
2023-09-17 01:37:19 +0800

    Add files via upload

---
+++ ShortStringException.java
@@ -0,0 +1,24 @@
+package com.utils.util;
+
+
+
+public class ShortStringException extends Exception {
+
+    public ShortStringException() {
+        super();
+    }
+
+    public ShortStringException(String message) {
+        super(message);
+    }
+
+    public ShortStringException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ShortStringException(Throwable cause) {
+        super(cause);
+    }
+
+}
+
